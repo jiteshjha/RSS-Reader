55964c314f45263106cadcaeb6010ff577ec7735
jiteshjha <jiteshjha96@gmail.com>
2016-03-11 17:04:54 +0530

    Added entries controller, entries view erb's

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
-  resources :feeds
-  root 'feeds#index'
+  resources :feeds do
+    member do
+      resources :entries, only: [:index, :show]
+    end
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
+  root 'feeds#index'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
